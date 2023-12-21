706e83f2a121d547fd6a6755de121d9476aea65a
Akash <sahuakash893@gmail.com>
2023-12-21 17:58:38 +0530

    Changed in json and index file

--- apiEstate/index.js
+++ apiEstate/index.js
@@ -4,8 +4,9 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.route.js';
-
 import cookieParser from 'cookie-parser';
+import path from 'path';
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -13,6 +14,9 @@ mongoose.connect(process.env.MONGO).then(()=>{
 }).catch((err)=>{
     console.log(err);
 })
+
+const __dirname = path.resolve();
+
 const app = express()
 app.use(express.json())
 app.use(cookieParser());
@@ -25,6 +29,11 @@ app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
 app.use('/api/listing', listingRouter);
 
+app.use(express.static(path.join(__dirname, '/clientEstate/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'clientEstate', 'dist', 'index.html'));
+})
 
 
 app.use((err, req, res, next) => {
