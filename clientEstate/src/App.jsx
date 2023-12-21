e09827a8abe8d3a3c62d095f876abb92a4709b88
Akash <sahuakash893@gmail.com>
2023-12-21 18:28:36 +0530

    Changed App.jsx

--- clientEstate/src/App.jsx
+++ clientEstate/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import SignIn from './pages/Signin';
-import SignUp from './pages/Signup';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 import About from './pages/About';
